f29d68e707fc35d38de0cd6bc16f009ee2f36258
Sukhwinder <singhdevelops@gmail.com>
2024-07-20 14:45:53 +0100

    added btn text prop

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,11 @@ import Hero from './components/ui/Hero'
 function App() {
   const heroTitle = (<><span className="text-[#f56551]">Discover your next adventure with AI:</span> personalised itineraries at your fingertips</>);
   const heroShortDescription = 'Your personal trip planner and travel curator, creating custom itineraries tailored to your interest and budget.';
+  const heroBtnText = 'Get started, it\'s free.';
 
   return (
     <>
-      <Hero title={heroTitle} shortDescription={heroShortDescription} />
+      <Hero title={heroTitle} shortDescription={heroShortDescription} btnText={heroBtnText} />
     </>
   )
 }
