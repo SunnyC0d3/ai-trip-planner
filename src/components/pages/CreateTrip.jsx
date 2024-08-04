af78132ea2c189a65913f8fb6c709d0e4e67b614
Sukhwinder <singhdevelops@gmail.com>
2024-08-04 13:18:39 +0100

    adding options

--- src/components/pages/CreateTrip.jsx
+++ src/components/pages/CreateTrip.jsx
@@ -1,15 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import GooglePlacesAutocomplete from 'react-google-places-autocomplete'
+import { Input } from '../ui/input';
 
 function CreateTrip() {
+  const [place, setPlace] = useState();
   return (
     <div className="sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10">
       <h2 className="font-bold text-3xl">Tell us your travel preferences</h2>
       <p className="mt-3 text-gray-500 text-xl">Just provide some basic information, and our trip planner will generate a customized itinerary based on your preferences.</p>
-      <div className="mt-20">
+      <div className="mt-20 flex flex-col gap-10">
         <div>
           <h2 className="text-xl my-3 font-medium">What is the destination of choice?</h2>
-          <GooglePlacesAutocomplete apiKey={import.meta.env.VITE_GOOGLE_PLACES_API_KEY} />
+          <GooglePlacesAutocomplete
+            apiKey={import.meta.env.VITE_GOOGLE_PLACES_API_KEY}
+            selectProps={{
+              place,
+              onChange: (value) => { setPlace(value) }
+            }}
+          />
+        </div>
+        <div>
+          <h2 className="text-xl my-3 font-medium">How many days are you planning your trip?</h2>
+          <Input placeholder={'Ex. 3'} type="number" />
+        </div>
+        <div>
+          <h2 className="text-xl my-3 font-medium">What is your budget?</h2>
+          <p>The budget is exclusively allocated for activities and dining purposes.</p>
+          <div></div>
         </div>
       </div>
     </div>
---
+++ src/constants/options.jsx
@@ -0,0 +1,33 @@
+export const SelectTravelsList = [
+    {
+        id: 1,
+        title: 'Just me',
+        desc: 'A soul travels in exploration',
+        icon: '&#9992;',
+        people: '1 person'
+    },
+    {
+        id: 2,
+        title: 'A couple',
+        desc: 'Two travels in tandem',
+        icon: '&#xf5ce;',
+        people: '2 people'
+    },
+];
+
+export const SelectBudgetOptions = [
+    {
+        id: 1,
+        title: 'Cheap',
+        desc: 'Stay concious of costs',
+        icon: '&#xf3d1;'
+    },
+    {
+        id: 2,
+        title: 'Moderate',
+        desc: 'Keep cost on the average side',
+        icon: '&#xf4d3;'
+    },
+];
+
+export const AI_PROMPT = 'Generate travel plan for location : {location}';
