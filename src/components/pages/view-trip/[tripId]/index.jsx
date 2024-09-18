ebbe967823c743ae8c6823870cd4a79b9a1b1e14
Sukhwinder <singhdevelops@gmail.com>
2024-09-18 10:25:28 +0100

    retrieving info from firebase

--- src/components/pages/view-trip/[tripId]/index.jsx
+++ src/components/pages/view-trip/[tripId]/index.jsx
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { db } from '@/service/FirebaseConfig';
+import { doc, getDoc } from 'firebase/firestore';
+import { useParams } from 'react-router-dom';
 
 function ViewTrip() {
+    const { tripId } = useParams();
+
+    async function getTripData() {
+        const docRef = doc(db, 'AI Trip Planner', tripId);
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+            console.log(docSnap.data());
+        } else {
+            toast('No trip found.');
+        }
+    }
+
+    useEffect(() => {
+        tripId && getTripData();
+    }, [tripId]);
+
     return (
         <div>ViewTrip</div>
     )
