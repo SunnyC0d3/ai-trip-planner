905b66066eb9301096e39afea3dc7c13b8276d56
Sukhwinder <singhdevelops@gmail.com>
2024-09-19 20:18:10 +0100

    building itinerary images

--- src/components/pages/view-trip/[tripId]/index.jsx
+++ src/components/pages/view-trip/[tripId]/index.jsx
@@ -2,17 +2,17 @@ import React, { useEffect, useState } from 'react';
 import { db } from '@/service/FirebaseConfig';
 import { doc, getDoc } from 'firebase/firestore';
 import { Link, useParams } from 'react-router-dom';
-import placeholderImg from '@/assets/placeholderImg.jpg'
 import { IoIosSend } from "react-icons/io";
 import { Button } from '@/components/ui/Button';
-import { getPlaceDetails } from '@/service/GlobalAPI';
-
-const PHOTO_REF_URL = `https://places.googleapis.com/v1/{NAME}/media?maxHeightPx=1000&maxWidthPx=1000&key=${import.meta.env.VITE_GOOGLE_PLACES_API_KEY}`;
+import { getPlaceDetails, PHOTO_REF_URL } from '@/service/GlobalAPI';
 
 function ViewTrip() {
     const { tripId } = useParams();
+
     const [trip, setTrip] = useState([]);
     const [photoUrl, setPhotoUrl] = useState();
+    const [hotelPhotoUrlArray, setHotelPhotoUrlArray] = useState([]);
+    const [itineraryPhotoUrlArray, setItineraryPhotoUrlArray] = useState([]);
 
     async function getTripData() {
         const docRef = doc(db, 'AI Trip Planner', tripId);
@@ -31,9 +31,51 @@ function ViewTrip() {
 
     useEffect(() => {
         trip && getPlacePhoto();
+
+        trip && trip?.tripData?.hotelOptions?.map((hotel, index) => {
+            async function getHotelPhotos(hotel) {
+                const data = {
+                    textQuery: hotel?.hotelName
+                }
+
+                const result = await getPlaceDetails(data).then((response) => {
+                    setHotelPhotoUrlArray((prevArray) => [...prevArray, PHOTO_REF_URL.replace('{NAME}', response.data.places[0].photos[3].name)]);
+                });
+            }
+
+            getHotelPhotos(hotel);
+        });
+
+        trip && trip?.tripData?.itinerary?.map((itinerary, itineraryIndex) => {
+            itinerary.plan.map((place, placeIndex) => {
+                async function getItineraryPlacePhotos() {
+                    const data = {
+                        textQuery: place?.placeName
+                    }
+
+                    const result = await getPlaceDetails(data).then((response) => {
+                        const photoUrl = PHOTO_REF_URL.replace('{NAME}', response.data.places[0].photos[3].name);
+
+                        setItineraryPhotoUrlArray((prevArray) => {
+                            const updatedArray = [...prevArray];
+
+                            if (!updatedArray[itineraryIndex]) {
+                                updatedArray[itineraryIndex] = [];
+                            }
+
+                            updatedArray[itineraryIndex].push(photoUrl);
+
+                            return updatedArray;
+                        });
+                    });
+                }
+
+                getItineraryPlacePhotos();
+            });
+        });
     }, [trip]);
 
-    const getPlacePhoto = async() => {
+    const getPlacePhoto = async () => {
         const data = {
             textQuery: trip?.userSelection?.location?.label
         }
@@ -42,6 +84,8 @@ function ViewTrip() {
             setPhotoUrl(PHOTO_REF_URL.replace('{NAME}', response.data.places[0].photos[3].name));
         });
     }
+
+    console.log(itineraryPhotoUrlArray);
 
     return (
         <>
@@ -64,7 +108,7 @@ function ViewTrip() {
                         {trip?.tripData?.hotelOptions?.map((hotel, index) => (
                             <Link key={index} to={`https://www.google.com/maps/search/?api=1&query=${hotel?.hotelName + " " + hotel?.hotelAddress}`} target="_blank">
                                 <div className="hover:scale-105 transition-all cursor-pointer">
-                                    <img src={hotel?.hotelImageUrl} className="rounded-lg" />
+                                    <img src={hotelPhotoUrlArray[index]} className="rounded-xl h-[180px] w-full object-cover" />
                                     <div className="my-2">
                                         <h3 className="font-small">{hotel?.hotelName}</h3>
                                         <p className="text-xs text-gray-400">📍 {hotel?.hotelAddress}</p>
@@ -79,17 +123,17 @@ function ViewTrip() {
                 <div>
                     <h2 className="font-bold text-xl my-5">Places to visit</h2>
                     <div>
-                        {trip?.tripData?.itinerary?.map((itinerary, index) => (
-                            <div className="mt-5" key={index}>
+                        {trip?.tripData?.itinerary?.map((itinerary, itineraryIndex) => (
+                            <div className="mt-5" key={itineraryIndex}>
                                 <h3 className="font-medium text-lg">Day {itinerary.day}</h3>
                                 <div className="grid md:grid-cols-2 gap-5">
                                     {
-                                        itinerary.plan.map((place, index) => (
-                                            <div className="my-3" key={index}>
+                                        itinerary.plan.map((place, placeIndex) => (
+                                            <div className="my-3" key={placeIndex}>
                                                 <p className="font-medium text-sm text-orange-600">{place?.bestTime}</p>
-                                                <Link key={index} to={`https://www.google.com/maps/search/?api=1&query=${place?.placeName + " " + place?.geoCoordinates[0] + " " + place?.geoCoordinates[1]}`} target="_blank">
+                                                <Link to={`https://www.google.com/maps/search/?api=1&query=${place?.placeName + " " + place?.geoCoordinates[0] + " " + place?.geoCoordinates[1]}`} target="_blank">
                                                     <div className="border rounded-xl p-3 mt-5 flex gap-5 hover:scale-105 hover:shadow-md transition-all cursor-pointer">
-                                                        <img className="w-[130px] h-[130px] rounded-xl" src="/placeholder.jpg" alt="" />
+                                                        {/* {itineraryPhotoUrlArray[itineraryIndex][placeIndex] && <img className="w-[130px] h-[130px] rounded-xl" src={itineraryPhotoUrlArray[itineraryIndex][placeIndex]} alt="" />} */}
                                                         <div>
                                                             <p className="font-bold text-lg mb-3">{place?.placeName}</p>
                                                             <p className="text-sm text-gray-400">{place?.placeDetails}</p>
